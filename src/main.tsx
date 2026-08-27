e4d3c5283c86f246ff8f28b55b6104530c25063e
Lucca Gibelli <115755176+LuccaGibelli@users.noreply.github.com>
2026-08-27 00:03:46 -0300

    Liga HUD operacional ao app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import IngestionStatus from './IngestionStatus'
 import './styles.css'
 import './enhancements.css'
+import './ingestion-status.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <IngestionStatus />
   </React.StrictMode>,
 )
